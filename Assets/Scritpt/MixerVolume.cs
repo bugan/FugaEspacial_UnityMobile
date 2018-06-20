56e85f89699cf01565eed50b31412f5505fe0f62
Ricardo <dones.bugan@gmail.com>
2018-06-20 16:41:39 -0300

    projeto inicial

---
+++ Assets/Scritpt/MixerVolume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerVolume : MonoBehaviour {
+
+    [SerializeField]
+    private AudioMixer mixer;
+    [SerializeField]
+    private string parametro;
+
+    public void MudarAudio(float config)
+    {
+        var volume = Mathf.Lerp(-60, 0, config);
+        this.mixer.SetFloat(parametro, volume);
+    }
+}
---
+++ Assets/Scritpt/SegueMouse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegueMouse : MonoBehaviour {
+	private void Update () {
+        var destino = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = new Vector3(destino.x, destino.y, this.transform.position.z);
+	}
+}

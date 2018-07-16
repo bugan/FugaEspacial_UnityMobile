ce9b464dded2ba0d1549912e9e7959751a1c484f
Bugan <dones.bugan@gmail.com>
2018-07-16 14:49:04 -0300

    Video 2.1

---
+++ Assets/Scritpt/Gameplay/ControlePause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ControlePause : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject painelPause;
+
+    private void Update()
+    {
+        if (EstaoTocandoNaTela())
+        {
+            this.ContinuarOJogo();
+        }
+        else
+        {
+            this.PararOJogo();
+        }
+    }
+
+    private bool EstaoTocandoNaTela()
+    {
+#if UNITY_EDITOR
+        return Input.GetKey(KeyCode.A);
+#elif UNITY_ANDROID
+        return Input.touchCount > 0;
+#endif
+    }
+
+    private void PararOJogo()
+    {
+        this.painelPause.SetActive(true);
+    }
+
+    private void ContinuarOJogo()
+    {
+        this.painelPause.SetActive(false);
+    }
+}

d17a2aefe24161a303b4421ecc702cd1aa28b7fb
Bugan <dones.bugan@gmail.com>
2018-07-18 15:44:45 -0300

    Ajustando interface Ranking

---
+++ Assets/Scritpt/UI/NovaPontuacao.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NovaPontuacao : MonoBehaviour {
+    private const int VALOR_DE_ERRO = -1;
+
+    [SerializeField]
+    private Text textoPontuacao;
+
+    private Pontuacao pontuacao;
+	
+	private void Start () {
+        this.pontuacao = GameObject.FindObjectOfType<Pontuacao>();
+
+        var totalDePontos = VALOR_DE_ERRO;
+
+        if (Existe(this.pontuacao))
+        {
+            totalDePontos = this.pontuacao.Pontos;
+        }
+
+        this.textoPontuacao.text = totalDePontos.ToString();
+	}
+	
+	private bool Existe(Object objeto)
+    {
+        return objeto != null;
+    }
+}
